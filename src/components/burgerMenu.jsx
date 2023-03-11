6fe4ad71b9d6f664089f4b2d42729ede054ec7d8
GgermanBoldyrev <boldyrev_german@mail.ru>
2023-03-11 11:59:35 +0300

    asd

--- src/components/burgerMenu.jsx
+++ src/components/burgerMenu.jsx
@@ -50,7 +50,8 @@ const Dropdown = styled.div`
 
   @media only screen and (max-width: 600px) {
     margin-right: 5px;
-    background-color: rgba(34, 34, 34, 0.8);
+    /*background-color: rgba(34, 34, 34, 0.8);*/
+    background-color: red;
   }
 `;
 
